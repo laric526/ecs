486b38766515f60c0692a8afa4e6d4bf7ce54062
laric526 <90987401+laric526@users.noreply.github.com>
2022-10-13 13:07:27 -0700

    added getEntities

--- example/example.js
+++ example/example.js
@@ -1,7 +1,9 @@
 import { World } from "../src/world.js";
 
-const test = {
-    num: Number
+const world = new World();
+world.entities = { 
+    1: { components: { position: { data: "data" }, "velocity": { data: "data" } } }, 
+    2: { components: { position: { data: "data" } } }, 
+    3: { components: { position: { data: "data" }, "velocity": { data: "data" }, "dummy": { data: "data" }  } }, 
 }
-
-alert("ayy");
+alert(world.getEntities(["position", "velocity"]).length);
--- src/world.js
+++ src/world.js
@@ -6,4 +6,19 @@ export function World() {
     this.registerComponent = function(name, schema) {
         this.components[name] = schema;
     }
+
+    this.getEntities = function(components) {
+        const out = [];
+
+        for (const entity in this.entities) {
+            const entityComponents = Object.getOwnPropertyNames(entity.components);
+            const valid = components.every(i => entityComponents.includes(i));
+            
+            if (valid) {
+                out.push(entity);
+            }
+        }
+
+        return out;
+    }
 }
