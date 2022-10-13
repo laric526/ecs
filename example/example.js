59fefb2cfb058aecfb15ed88e25117d6f971fad5
laric526 <90987401+laric526@users.noreply.github.com>
2022-10-13 13:16:54 -0700

    again

--- example/example.js
+++ example/example.js
@@ -4,6 +4,6 @@ const world = new World();
 world.entities = { 
     1: { components: { position: { data: "data" }, velocity: { data: "data" } } }, 
     2: { components: { position: { data: "data" } } }, 
-    3: { components: { position: { data: "data" }, velocity: { data: "data" }, dummy: { data: "data" }  } }, 
+    3: { components: { position: { data: "data" }, velocity: { data: "data" }, dummy: { data: "data" } } }
 };
 alert(world.getEntities(["position", "velocity"]).length);
