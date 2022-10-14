3fe4ef1534ad9165a2b3622487edde9d368e870f
laric526 <90987401+laric526@users.noreply.github.com>
2022-10-14 10:09:49 -0700

    added console override (no devtools on chromeboi)

---
+++ example/console_override.js
@@ -0,0 +1,17 @@
+const consoleElement = document.getElementById("console");
+
+if (window.console && console) {
+    for (const c in console) {
+        if (typeof console[c] === 'function') {
+            const cx = console[c]
+            console[c] = function () {
+                const li = document.createElement("li");
+                li.innerHTML = arguments;
+                consoleElement.appendChild(li);
+                cx.apply(this, arguments);
+            }
+        }
+    }
+}
+
+console.log("test!");
