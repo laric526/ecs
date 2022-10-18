3fdd6360b7622a76c5ca33ea23f2251ee3f4c091
laric526 <90987401+laric526@users.noreply.github.com>
2022-10-18 10:20:04 -0700

    formatting

--- tanks/src/util/sprites.js
+++ tanks/src/util/sprites.js
@@ -7,16 +7,16 @@ export function loadSprites(callback) {
         "http://placekitten.com/85/150",
         "http://placekitten.com/85/130",
         "http://placekitten.com/85/110",
-      ];
-      Promise.all(imageUrls.map(loadImage)).then(images => {
-          const canvas = document.createElement("canvas");
-          document.body.appendChild(canvas);
-          const ctx = canvas.getContext("2d");
-          images.forEach((image, i) => {
-              const url = imageUrls[i];
-              console.log(`Loaded image: ${url}`);
-          });
-      });
+    ];
+    Promise.all(imageUrls.map(loadImage)).then(images => {
+        const canvas = document.createElement("canvas");
+        document.body.appendChild(canvas);
+        const ctx = canvas.getContext("2d");
+        images.forEach((image, i) => {
+            const url = imageUrls[i];
+            alert(`Loaded image: ${url}`);
+        });
+    });
 }
 
 function loadImage(src) {
