c6e19fedbfcef1ca6c6fa2f5d5f131de71cac4ec
laric526 <90987401+laric526@users.noreply.github.com>
2022-10-19 09:30:24 -0700

    remove logs

--- tanks/src/systems/movement.js
+++ tanks/src/systems/movement.js
@@ -3,12 +3,9 @@ import { System } from "../../../src/system.js";
 export const movementSystem = new System("movement",
     ["rotation", "velocity", "input", "player_controlled"],
     (entity) => {
-        console.log(entity);
         const direction = entity.components.rotation.angle * entity.components.input.axes.longitudinal;
-        console.log(direction);
 
         entity.components.velocity.x = Math.sin(direction);
         entity.components.velocity.y = -Math.cos(direction);
-        console.log(entity);
     }
 );
