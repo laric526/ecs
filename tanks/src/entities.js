11cc5c3c35a900e701047ded0682feb04b640c2c
laric526 <90987401+laric526@users.noreply.github.com>
2022-10-19 09:08:35 -0700

    added new components to test entity

--- tanks/src/entities.js
+++ tanks/src/entities.js
@@ -1,3 +1,5 @@
 import { Entity } from "../../src/entity.js";
 
-export const testrender = new Entity(["position", "rotation", "sprite", "render"]);
+export const testrender = new Entity(
+    ["position", "rotation", "velocity", "sprite", "render", "input", "player_controlled"]
+);
