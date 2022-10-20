5c60a2f4a455f6da517e3910230c14251958069f
laric526 <90987401+laric526@users.noreply.github.com>
2022-10-20 11:59:26 -0700

    check every step of point at mouse system

--- tanks/src/systems/pointAtMouse.js
+++ tanks/src/systems/pointAtMouse.js
@@ -1,12 +1,17 @@
 import { System } from "../../../src/system.js";
 import { mouse } from "../util/mouse.js";
 
-export const pointAtMouseSystem = new System("point_at_mouse", ["position", "rotation", "point_at_mouse"], (entity) => {
+export const pointAtMouseSystem = new System("point_at_mouse", ["position", "rotation", "point_at_mouse"], (entity) => { 
+    console.log(`Getting X and Y from entity...`);
     const x = entity.component.position.x;
     const y = entity.component.position.y;
+    console.log(`Got (${x}, ${y})`);
 
+    console.log(`Calculating angle...`);
     const angle = Math.atan2(mouse.pos.y - y, mouse.pos.x - x) + Math.PI / 2;
-    console.log(angle);
+    console.log(`Calculated ${angle}`);
 
+    console.log(`Applying angle...`);
     entity.component.rotation.angle = angle;
+    console.log(`Applied. Entities angle is now ${entity.component.rotation.angle}`);
 });
