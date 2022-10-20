63546ed7fe3f2ca722f659c39aad358be052211d
laric526 <90987401+laric526@users.noreply.github.com>
2022-10-20 12:06:50 -0700

    change cannon sprite

--- tanks/src/entities.js
+++ tanks/src/entities.js
@@ -15,6 +15,6 @@ export const playerGunEntity = new Entity(
     ["attachment", "position", "rotation", "sprite", "attached", "point_at_mouse", "render"],
     (entity) => {
         entity.components.attachment.parent = playerEntity.id; // this is bad lmao
-        entity.components.sprite.sprite = "32";
+        entity.components.sprite.sprite = "cannon";
     }
 );
