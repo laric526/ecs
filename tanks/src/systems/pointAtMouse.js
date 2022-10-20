0de67ce789cfdfa37ac0cef41ce7d17481380607
laric526 <90987401+laric526@users.noreply.github.com>
2022-10-20 12:00:54 -0700

    component -> components

--- tanks/src/systems/pointAtMouse.js
+++ tanks/src/systems/pointAtMouse.js
@@ -3,8 +3,8 @@ import { mouse } from "../util/mouse.js";
 
 export const pointAtMouseSystem = new System("point_at_mouse", ["position", "rotation", "point_at_mouse"], (entity) => { 
     console.log(`Getting X and Y from entity...`);
-    const x = entity.component.position.x;
-    const y = entity.component.position.y;
+    const x = entity.components.position.x;
+    const y = entity.components.position.y;
     console.log(`Got (${x}, ${y})`);
 
     console.log(`Calculating angle...`);
@@ -12,6 +12,6 @@ export const pointAtMouseSystem = new System("point_at_mouse", ["position", "rot
     console.log(`Calculated ${angle}`);
 
     console.log(`Applying angle...`);
-    entity.component.rotation.angle = angle;
-    console.log(`Applied. Entities angle is now ${entity.component.rotation.angle}`);
+    entity.components.rotation.angle = angle;
+    console.log(`Applied. Entities angle is now ${entity.components.rotation.angle}`);
 });
