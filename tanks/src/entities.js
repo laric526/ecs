a88392d0fcb158ff993cf7604b274da1c3bf7bf9
laric526 <90987401+laric526@users.noreply.github.com>
2022-10-20 17:42:45 -0700

    upload new sprite

--- tanks/src/entities.js
+++ tanks/src/entities.js
@@ -3,7 +3,7 @@ import { Entity } from "../../src/entity.js";
 export const playerEntity = new Entity(
     ["input", "position", "rotation", "speed", "rotation_speed", "sprite", "velocity", "rotation_velocity", "player_controlled", "render"],
     (entity) => {
-        entity.components.sprite.sprite = "tank";
+        entity.components.sprite.sprite = "tanks/red";
         entity.components.position.x = 48;
         entity.components.position.y = 24;
         entity.components.speed.speed = 2;
@@ -15,6 +15,6 @@ export const playerGunEntity = new Entity(
     ["attachment", "position", "rotation", "sprite", "attached", "point_at_mouse", "render"],
     (entity) => {
         entity.components.attachment.parent = playerEntity.id; // this is bad lmao
-        entity.components.sprite.sprite = "cannon";
+        entity.components.sprite.sprite = "tanks/red_cannon";
     }
 );
