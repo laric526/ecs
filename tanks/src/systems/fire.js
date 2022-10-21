dff866673d92177feb98c27b230abf28e446fd92
laric526 <90987401+laric526@users.noreply.github.com>
2022-10-21 03:04:13 -0700

    set missile position and verk jankily stop firing

--- tanks/src/systems/fire.js
+++ tanks/src/systems/fire.js
@@ -2,13 +2,19 @@ import { System } from "../../../src/system.js";
 import { duplicateEntity } from "../../../src/entity.js";
 import { missileEntity } from "../entities.js";
 
+var fired = false;
+
 export const fireSystem = new System("fire", ["input", "position", "rotation", "tank", "player_controlled"], (entity, world) => {
-    if (entity.components.input.fire) {
+    if (entity.components.input.fire && !fired) {
         const cannon = world.getEntity(entity.components.tank.cannon);
         const angle = cannon.components.rotation.angle;
 
         const missile = world.createEntity(duplicateEntity(missileEntity));
+        missile.components.position.x = entity.components.position.x;
+        missile.components.position.y = entity.components.position.y;
         missile.components.rotation.angle = angle;
         missile.components.velocity.y = 1;
+
+        fired = true;
     }
 });
