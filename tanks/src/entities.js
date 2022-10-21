ebf14e98ace60d8b0d02f33f1da31239443e8a81
laric526 <90987401+laric526@users.noreply.github.com>
2022-10-20 19:05:28 -0700

    maybe i should read the error messages

--- tanks/src/entities.js
+++ tanks/src/entities.js
@@ -1,16 +1,8 @@
 import { Entity } from "../../src/entity.js";
 
-const playerCannonEntity = new Entity(
-    ["attachment", "position", "rotation", "sprite", "attached", "point_at_mouse", "render"],
-    (entity) => {
-        entity.components.attachment.parent = playerEntity.id;
-        entity.components.sprite.sprite = "tanks/red_cannon";
-    }
-);
-
 export const playerEntity = new Entity(
     ["input", "position", "rotation", "speed", "rotation_speed", "sprite", "velocity", "rotation_velocity", "player_controlled", "render"],
-    (entity), world => {
+    (entity, world) => {
         entity.components.sprite.sprite = "tanks/red";
         entity.components.position.x = 48;
         entity.components.position.y = 24;
@@ -18,5 +10,13 @@ export const playerEntity = new Entity(
         entity.components.rotation_speed.speed = 3.5;
 
         world.createEntity(playerCannonEntity);
+    }
+);
+
+const playerCannonEntity = new Entity(
+    ["attachment", "position", "rotation", "sprite", "attached", "point_at_mouse", "render"],
+    (entity) => {
+        entity.components.attachment.parent = playerEntity.id;
+        entity.components.sprite.sprite = "tanks/red_cannon";
     }
 );
