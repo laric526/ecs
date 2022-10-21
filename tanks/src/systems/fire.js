652985f8a6f3c01e6b7a3da075da9ed3627f7160
laric526 <90987401+laric526@users.noreply.github.com>
2022-10-21 12:23:59 -0700

    remove velocity from missile to test firing position

--- tanks/src/systems/fire.js
+++ tanks/src/systems/fire.js
@@ -17,6 +17,13 @@ export const fireSystem = new System("fire", ["input", "position", "rotation", "
         missile.components.position.x = missile.components.velocity.x * 8 + entity.components.position.x;
         missile.components.position.y = missile.components.velocity.y * 8 + entity.components.position.y;
 
+
+
+        missile.components.velocity.x = 0;
+        missile.components.velocity.y = 0;
+
+
+
         missile.components.rotation.angle = angle;
 
         entity.components.tank.reloadTimer = entity.components.tank.reloadTime;
