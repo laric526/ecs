085e1c8d45f36c2eeb91b1a09445654e483a615f
laric526 <90987401+laric526@users.noreply.github.com>
2022-10-24 11:23:40 -0700

    load map

---
+++ tanks/src/util/map.js
@@ -0,0 +1,12 @@
+import { tileEntity } from "../entities/tile.js";
+import { duplicateEntity } from "../../../src/entity.js";
+
+export function loadMap(map, world) {
+    map.forEach((row, y) => {
+        row.forEach((tile, x) => {
+            const entity = world.createEntity(duplicateEntity(tileEntity));
+            entity.components.position.x = x * 16 + 8;
+            entity.components.position.y = y * 16 + 8;
+        });
+    });
+}
