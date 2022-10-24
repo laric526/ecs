3676385935df0595558d5834e10c77a50d26264a
laric526 <90987401+laric526@users.noreply.github.com>
2022-10-24 11:30:56 -0700

    bruh

--- tanks/src/util/map.js
+++ tanks/src/util/map.js
@@ -4,9 +4,12 @@ import { duplicateEntity } from "../../../src/entity.js";
 export function loadMap(map, world) {
     map.forEach((row, y) => {
         row.forEach((tile, x) => {
-            const entity = world.createEntity(duplicateEntity(tileEntity));
-            entity.components.position.x = x * 16 + 8;
-            entity.components.position.y = y * 16 + 8;
+            switch(tile) {
+                case 1: // walll
+                    const entity = world.createEntity(duplicateEntity(tileEntity));
+                    entity.components.position.x = x * 16 + 8;
+                    entity.components.position.y = y * 16 + 8;
+            }
         });
     });
 }
