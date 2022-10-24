57867402e4eab5d3ad9be3553da1088b4dfd7813
laric526 <90987401+laric526@users.noreply.github.com>
2022-10-24 13:43:48 -0700

    test collision rewrite

--- tanks/src/systems/collision.js
+++ tanks/src/systems/collision.js
@@ -40,6 +40,42 @@ function handleTankCollision(entity, other, world) {
             const xOther = other.components.position.x;
             const yOther = other.components.position.y;
 
+            const angle = entity.components.rotation.angle;
+            const xStep = Math.sin(angle);
+            const yStep = -Math.cos(angle);
+
+            const xSign = xStep != 0 ? Math.sign(xStep) : 1;
+            const ySign = yStep != 0 ? Math.sign(yStep) : 1; 
+            
+            const xCount = (xOther + (xSign * 8) - x) / xStep;
+            const yCount = (yOther + (ySign * 8) - y) / yStep;
+
+            var count;
+            if (Math.abs(xCount) < Math.abs(yCount)) { count = Math.abs(xCount); }
+            else { count = Math.abs(yCount); }
+            
+            const xNew = x + xStep * count;
+            const yNew = y + yStep * count;    
+            
+            entity.components.position.x = xNew;
+            entity.components.position.y = yNew;
+
+            console.log(`Previous Position: (${( Math.sin(entity.components.rotation.angle) * -entity.components.speed.speed ).toFixed(4)}, ${(-Math.cos(entity.components.rotation.angle) * -entity.components.speed.speed ).toFixed(4)})`);
+
+            break;
+    }
+}
+
+function handleTankCollisionold(entity, other, world) {
+    switch(other.components.collider.type) {
+        case "tank":
+        case "wall":
+        case "hole":
+            const x = entity.components.position.x;
+            const y = entity.components.position.y;
+            const xOther = other.components.position.x;
+            const yOther = other.components.position.y;
+
             const angle = Math.atan2(y - yOther, x - xOther) + Math.PI / 2;
             //const angle = entity.components.rotation.angle;
             const xStep = Math.sin(angle);
